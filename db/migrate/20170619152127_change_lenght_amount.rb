99f79a6c1476e74b732b8e5cb41757581459204c
stebneruslan <ruslanbilmakovskiy@gmail.com>
2017-06-20 13:12:36 +0300

    init

--- db/migrate/20170619152127_change_lenght_amount.rb
+++
@@ -1,9 +0,0 @@
-class ChangeLenghtAmount < ActiveRecord::Migration
-  def up
-    add_column :balances, :amount, :decimal,  precision: 15, scale: 3
-  end
-
-  def down
-    remove_column :balances, :amount
-  end
-end
